07e294ed80007c77ce829d54fb1e61b18883ef7d
Jasper Jan Tan <jasperjantan1996@gmail.com>
2024-03-26 00:19:30 -0400

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://micro-data1-jaspers-projects-f11df7d9.vercel.app/",
+    publicPath: "https://micro-data1-git-main-jaspers-projects-f11df7d9.vercel.app/",
   },
 
   resolve: {
